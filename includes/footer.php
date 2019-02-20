c630754cc436abc0ffbe70c6b9ea3427e75d3a71
cochrand84 <david@davidcochran.us>
2019-02-20 12:00:57 -0600

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,9 +4,9 @@
 </br>
 </br>
 </br>
-<a href="ticket.php" class="linkbutton">Create a new ticket</a>
+
 <a href="templates/password_protect.php?logout=1" class="linkbutton">Logout</a>
 <a href="index.php" class="linkbutton">Back to home</a>
+<a href="ticket.php" class="linkbutton">Create a new ticket</a>
 
-<script src="lightbox/js/lightbox-plus-jquery.js">
 </html>
