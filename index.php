725af89d7045a63fa4bab7364281c7f13dfc41f5
cochrand84 <david@davidcochran.us>
2019-02-20 12:02:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php 
+require "includes/header.php";
+include "includes/password_protect.php"; 
+require "includes/config.php";
+require "includes/common.php";  
 require "includes/footer.php"; 
 ?>
