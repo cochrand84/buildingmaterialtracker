20301ee68f7045ea6f04197d4395aa52b6ac41f0
cochrand84 <david@davidcochran.us>
2019-02-20 11:59:55 -0600

    adding footer to index

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,7 @@
 </br>
 </br>
 </br>
+<a href="ticket.php" class="linkbutton">Create a new ticket</a>
 <a href="templates/password_protect.php?logout=1" class="linkbutton">Logout</a>
 <a href="index.php" class="linkbutton">Back to home</a>
 
