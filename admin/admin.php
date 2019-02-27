fc3b0ee826c660ff3f80d72980ec540c0789aa21
cochrand84 <david@davidcochran.us>
2019-02-27 13:43:32 -0600

    version and admin

--- admin/admin.php
+++ admin/admin.php
@@ -24,9 +24,37 @@ if (isset($_POST['verup'])) {
 }
 
 
+if (isset($_POST['dropalltables'])) {
+    try  {
+        
+        require "../includes/config.php";
+        require "../includes/common.php";
+
+        $connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "EXEC tickets 'ALTER TABLE ? NOCHECK CONSTRAINT all'";
+
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+    } catch(PDOException $error) {
+        echo $sql . "<br>" . $error->getMessage();
+    }
+}
+
+if (isset($_POST['installtables'])) {
+    try  {
+        
+        include "install.php";
+
+    } 
+}
 
 ?>
 
 <form method="post">
     <input type="submit" name="verup" value="verup">
+    <input type="submit" name="dropalltables" value="dropalltables">
+    <input type="submit" name="installtables" value="installtables">
 </form>
--- admin/password_protect.php
+++ admin/password_protect.php
@@ -63,7 +63,7 @@ define('USE_USERNAME', true);
 define('LOGOUT_URL', 'http://lakeside.davidcochran.us/index.php');
 
 // time out after NN minutes of inactivity. Set to 0 to not timeout
-define('TIMEOUT_MINUTES', 1440);
+define('TIMEOUT_MINUTES', 10);
 
 // This parameter is only useful when TIMEOUT_MINUTES is not zero
 // true - timeout time from last activity, false - timeout time from login
