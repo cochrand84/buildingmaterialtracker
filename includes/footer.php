bb1d0770fd5fb1f8990feb6ff6aaec1adbe73750
cochrand84 <david@davidcochran.us>
2019-02-27 13:20:49 -0600

    update 0.6.5

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,22 @@
 </body>
+<?php
 
+require "../config.php";
+require "../common.php";
+
+$connection = new PDO($dsn, $username, $password, $options);
+
+        $sql = "SELECT * FROM tickets WHERE NOT (status = 'complete') ORDER by due_date ASC";
+
+        $location = $_POST['location'];
+
+        $statement = $connection->prepare($sql);
+        $statement->bindParam(':location', $location, PDO::PARAM_STR);
+        $statement->execute();
+
+        $result = $statement->fetchAll();
+
+        ?>
 </br>
 </br>
 </br>
@@ -9,4 +26,9 @@
 <a href="index.php" class="linkbutton">Back to home</a>
 <a href="ticket.php" class="linkbutton">Create a new ticket</a>
 <a href="coil.php" class="linkbutton">Coil weight and Footage Chart</a>
+
+</br>
+</br>
+
+
 </html>
