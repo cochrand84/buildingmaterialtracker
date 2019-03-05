75ee44b661d7c00f0d63859efb741c78024e233a
cochrand84 <david@davidcochran.us>
2019-03-05 14:19:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,21 @@
-<?php 
-require "includes/header.php";
+<link rel="stylesheet" href="css/style.css">
+<?php include "includes/header.php"; 
 include "includes/password_protect.php"; 
-?>
- 
-<div class="container">
-<form method="get" action="search.php"enctype="multipart/form-data">
-<div class="row">
-      <div class="col-25">
-        <label for="search">Search (Coil # or Customer Lastname)</label>
-      </div>
-      <div class="col-75">
-        <input type="text" name="editid" id="editid">
-      </div>
-    </div>
-</form>
-</div>
 
- <div class="row">
-      <input type="submit" value="Submit">
+?>
+<form method="get" action="edit2.php"enctype="multipart/form-data">
+<div class="row">     
+        <div class="col-25">
+            <label for="editid">Ticket number or select below</label>
+        </div>
+        <div class="col-25">
+            <input type="text" name="editid" id="editid">
+        </div>
+    </div> 
+    <div class="row">
+        <input type="submit" value="Submit">
     </div>
+        </form>
 <?php 
  
 require "includes/footer.php"; 
